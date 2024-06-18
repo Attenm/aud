663c92727d2eaf4419b5d208eb5ce2259b82ab2c
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-06-19 01:25:38 +0300

    tying to figure out is it working at all

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+app.get('/', (req, res)=>{
+    res.send(
+        '<h2>ok</h2>'
+    )
+})
+
 app.listen(PORT, ()=>{
     console.log('server is running');
 })
