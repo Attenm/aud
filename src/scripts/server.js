1d8d17d18baa0d5f95e46d1fd3f01a7e182f8bc0
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-06-19 01:44:16 +0300

    something

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res)=>{
+app.get('/text', (req, res)=>{
     res.send(
         '<h2>ok</h2>'
     )
