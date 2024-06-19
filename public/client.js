f1e76915ef2e6da3aaaced79d98bd256eaf04951
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-06-19 21:02:46 +0300

    created server and retrieve music list

---
+++ public/client.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const getSongsBtn = document.getElementById('get-songs');
+
+    const findSongs = async () => {
+        try {
+            const response = await fetch('/audio');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            return data.result;
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    };
+
+    const displaySongs = (songs) => {
+        const emptyBox = document.getElementById('empty');
+        const songsListHtml = '<ul class="songs__list"></ul>'
+        emptyBox.insertAdjacentHTML('afterend', songsListHtml);
+        emptyBox.remove();
+
+        let audiosHtml = '';
+        songs.forEach(song => {
+            const sourseHtml = `<source src="${song.path}" type="audio/mpeg">`
+            const audioHtml = `<label><audio controls>${sourseHtml}</audio>${song.name}</label>`
+            const liHtml = `<li class="song__item">${audioHtml}</li>`;
+            audiosHtml += liHtml;
+        });
+        const songsListElem = document.querySelector('.songs__list');
+        songsListElem.insertAdjacentHTML('afterbegin', audiosHtml);
+    };
+
+    const clickHandler = async () => {
+        const songs = await findSongs();
+        displaySongs(songs);
+    };
+
+    getSongsBtn.addEventListener('click', clickHandler);
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res)=>{
+    const dirName = path.join(__dirname, 'index.html')
+    fs.readFile(dirName, (err, data) => {
+        if(err){
+            throw new Error(err);
+        }
+        res.write(data);
+    });
+})
+
+app.get('/audio', (req, res) => {
+    const folderPath = path.join(__dirname, 'public', 'audio');
+    console.log(folderPath)
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Could not read list of songs' });
+        }
+        files.map(fileName => {
+            console.log(fileName);
+        })
+        const songs = files.map(fileName => {
+            return {
+                path: `audio/${encodeURIComponent(fileName)}`,
+                name: fileName
+            };
+        });
+        res.json({result: songs});
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
+
